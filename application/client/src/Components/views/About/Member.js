92506bb5131c7c9042ec6d61155935409cb57a14
Hesham Saleh <hsaleh88@gmail.com>
2020-11-14 22:33:33 +0200

    Rebased branch on dev and updated Routes file

--- application/client/src/Components/views/About/Member.js
+++ application/client/src/Components/views/About/Member.js
@@ -3,6 +3,7 @@ import { Container, Row, Col, Image, Button } from "react-bootstrap";
 import "./css/Member.css";
 import placeholder from "../../../Components/assets/Stock_img.png";
 import keith from "../../../Components/assets/Keith.png"
+import joy from "../../../Components/assets/Joy.png"
 
 export default function Member(props) {
 
@@ -29,7 +30,7 @@ export default function Member(props) {
       caption: "Hi, I am Joy, nice to meet you!",
       bio: "I graduated with a Master's degree in Education at the University of Pennsylvania. Before that, I studied psychology at Tsinghua University in China. This is my 6th year here in the U.S. Now my major is CS and this is my first semester. My skills lie in UX research, design, and front-end programming (still learning!). I'm interested in playing board-game, solving math puzzles, shooting videos about my cat, and Chinese watercolor painting.",
       linkedIn: "https://www.linkedin.com/in/zhuozhuo/",
-      img: placeholder,
+      img: joy,
     },
     {
       name: "Trenton Smith",

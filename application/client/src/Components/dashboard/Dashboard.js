4b6b4d548114e17dda448657c6b2dc37189f2789
Hesham Saleh <hsaleh88@gmail.com>
2020-11-22 14:16:06 +0200

    Added admin flag to auth

--- application/client/src/Components/dashboard/Dashboard.js
+++ application/client/src/Components/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import DashboardListings from "./DashboardListings";
 import DashboardMessages from "./DashboardMessages";
 
 export default function Dashboard(props) {
+
   return (
     <Container className="dashboard container" style={{ marginTop: "2rem" }}>
       <h3>My Dashboard</h3>

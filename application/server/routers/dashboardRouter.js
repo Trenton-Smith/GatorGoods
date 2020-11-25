61ce80fa3f44e547c5f9211fb370b04de34d4fc9
Hesham Saleh <hsaleh88@gmail.com>
2020-11-25 16:21:56 +0200

    Created admin dashboard controller and added queries to retrieve pending, approved and rejected products

--- application/server/routers/dashboardRouter.js
+++ application/server/routers/dashboardRouter.js
@@ -15,11 +15,40 @@ const {
  *          Dashboard.js -> DashboardListingCard.js.
  * Authors: YG, Trenton
  */
+const express = require("express");
+const router = express.Router();
+
+//===========================================
+//                 Dashboard
+//===========================================
+
+const {
+  getMyProducts,
+  deleteMyProduct,
+} = require("../controllers/dashboardController");
 
 router.post("/getMyProducts", getMyProducts); // loads all listings into the dashboard
 // router.post("/updateMyProduct", updateMyProduct); // not currently implemented
 router.post("/deleteMyProduct", deleteMyProduct); // changes visibility to 0 in db for a listing
 router.post("/getMyOffers", getMyOffers); // loads any messages from prospective buyers
 router.post("/getMyEmail", getMyEmail); // for rendering email specific to user
+
+//===========================================
+//                 Admin Dashboard
+//===========================================
+
+const {
+  getAllPendingProducts,
+  getAllApprovedProducts,
+  getAllRejectedProducts,
+  approveProduct,
+  rejectProduct,
+} = require("../controllers/adminDashboardController");
+
+router.post("/getAllPendingProducts", getAllPendingProducts);
+router.post("/getAllApprovedProducts", getAllApprovedProducts);
+router.post("/getAllRejectedProducts", getAllRejectedProducts);
+router.post("/approveProduct", approveProduct);
+router.post("/rejectProduct", rejectProduct);
 
 module.exports = router;

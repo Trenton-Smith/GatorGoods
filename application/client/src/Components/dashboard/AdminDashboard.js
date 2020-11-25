dead4dcbc1947f8ff2baf3078dadd7fe768e957b
Hesham Saleh <hsaleh88@gmail.com>
2020-11-25 16:20:08 +0200

    Refactored Dashboard to render 2 different Dashboard components based on user type

---
+++ application/client/src/Components/dashboard/AdminDashboard.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Container, Tab, Tabs } from "react-bootstrap";
+import AdminDashboardListings from "./AdminDashboardListings";
+
+export default function AdminDashboard(props) {
+  
+  return (
+      <>
+        <h3>Admin Dashboard</h3>
+        <Container style={{ paddingTop: "2rem" }}>
+            <Tabs
+                defaultActiveKey="listings"
+                transition={false}
+                id="noanim-tab-example"
+            >
+                <Tab eventKey="pending" title="Pending">
+                    <AdminDashboardListings status="pending"/>
+                </Tab>
+                <Tab eventKey="approved" title="Approved">
+                    <AdminDashboardListings status="approved"/>
+                </Tab>
+                <Tab eventKey="rejected" title="Rejected">
+                    <AdminDashboardListings status="rejected"/>
+                </Tab>
+            </Tabs>
+        </Container>
+      </>
+  );
+}
--- application/client/src/Components/dashboard/Dashboard.js
+++ application/client/src/Components/dashboard/Dashboard.js
@@ -2,26 +2,14 @@ import React from "react";
 import { Container, Tab, Tabs } from "react-bootstrap";
 import DashboardListings from "./DashboardListings";
 import DashboardMessages from "./DashboardMessages";
+import UserDashboard from "./UserDashboard";
+import AdminDashboard from "./AdminDashboard";
 
 export default function Dashboard(props) {
 
   return (
     <Container className="dashboard container" style={{ marginTop: "2rem" }}>
-      <h3>My Dashboard</h3>
-      <Container style={{ paddingTop: "2rem" }}>
-        <Tabs
-          defaultActiveKey="listings"
-          transition={false}
-          id="noanim-tab-example"
-        >
-          <Tab eventKey="listings" title="Listings">
-            <DashboardListings user={props.user} />
-          </Tab>
-          <Tab eventKey="messages" title="Messages">
-            <DashboardMessages />
-          </Tab>
-        </Tabs>
-      </Container>
+      {props.admin == "1" ? <AdminDashboard /> : <UserDashboard />}
     </Container>
   );
 }

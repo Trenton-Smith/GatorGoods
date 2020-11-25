bb76686c81342f4610634d99549a99c803f2568b
Hesham Saleh <hsaleh88@gmail.com>
2020-11-25 16:20:08 +0200

    Refactored Dashboard to render 2 different Dashboard components based on user type

---
+++ application/client/src/Components/dashboard/UserDashboard.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Container, Tab, Tabs } from "react-bootstrap";
+import DashboardListings from "./DashboardListings";
+import DashboardMessages from "./DashboardMessages";
+
+export default function UserDashboard(props) {
+  
+  return (
+      <>
+        <h3>My Dashboard</h3>
+        <Container style={{ paddingTop: "2rem" }}>
+            <Tabs
+                defaultActiveKey="listings"
+                transition={false}
+                id="noanim-tab-example"
+            >
+                <Tab eventKey="listings" title="Listings">
+                <DashboardListings user={props.user} />
+                </Tab>
+                <Tab eventKey="messages" title="Messages">
+                <DashboardMessages />
+                </Tab>
+            </Tabs>
+        </Container>
+      </>
+  );
+}

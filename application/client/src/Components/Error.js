c6c32e140c00fa96e0395e10c11b3eebd2b7da8d
Blin <blinvarfi@gmail.com>
2020-12-16 19:40:06 +0100

    create error 404 page

---
+++ application/client/src/Components/Error.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import {
+    Container,
+    Col,
+    Row
+} from "react-bootstrap";
+import "./Category.css"
+
+
+export default function Error() {
+    return (
+        <div>
+            <Container style={{ marginTop: "2rem" }}>
+                <Col>
+                    <Row>
+                        <Col style={{ textAlign: "center" }}>
+                            <h1>Error 404</h1>
+                            <p>The url you are looking for is not valid!</p>
+                        </Col>
+                    </Row>
+                </Col>
+            </Container>
+        </div>
+    );
+}

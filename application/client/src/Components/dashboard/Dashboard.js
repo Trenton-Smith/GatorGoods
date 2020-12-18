3017006d187a24f90fa2ac3742b7a46161f1a9c0
Hesham Saleh <hsaleh88@gmail.com>
2020-12-18 03:39:51 +0200

    Fixed dashboard

--- application/client/src/Components/dashboard/Dashboard.js
+++ application/client/src/Components/dashboard/Dashboard.js
@@ -1,31 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { Container, Tab, Tabs } from "react-bootstrap";
-import DashboardListings from "./DashboardListings";
-import DashboardMessages from "./DashboardMessages";
+import React from "react";
+import { Container } from "react-bootstrap";
 import UserDashboard from "./UserDashboard";
 import AdminDashboard from "./AdminDashboard";
 
 export default function Dashboard(props) {
-  /*
-     State and associated method for updating the state in order to render the users' emails on their unique dashboard
-     view. It is called automatically prior to user interaction with the view.
-    */
-  const [email, setEmail] = useState([]);
-  useEffect(() => {
-    axios
-      .post("/api/dashboard/getMyEmail", {
-        user_id: props.user,
-      })
-      .then(async (res) => {
-        // this is somewhat redundant as only 1 email is linked per user; however, it works - will change...
-        // later if a better method is found
-        for (const k of res.data.values()) {
-          console.log(k.email);
-          setEmail(k.email);
-        }
-      });
-  }, [props]);
-
   return (
     <Container className="dashboard container" style={{ marginTop: "2rem" }}>
       {props.admin === "1" ? <AdminDashboard user={props.user}/> : <UserDashboard user={props.user}/>}
--- application/client/src/Components/dashboard/UserDashboard.js
+++ application/client/src/Components/dashboard/UserDashboard.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Tab, Tabs } from "react-bootstrap";
 import DashboardListings from "./DashboardListings";
 import DashboardMessages from "./DashboardMessages";
+import axios from "axios";
 
 export default function UserDashboard(props) {
   
+  /*
+     State and associated method for updating the state in order to render the users' emails on their unique dashboard
+     view. It is called automatically prior to user interaction with the view.
+    */
+   const [email, setEmail] = useState([]);
+   useEffect(() => {
+     axios
+       .post("/api/dashboard/getMyEmail", {
+         user_id: props.user,
+       })
+       .then(async (res) => {
+         // this is somewhat redundant as only 1 email is linked per user; however, it works - will change...
+         // later if a better method is found
+         for (const k of res.data.values()) {
+           console.log(k.email);
+           setEmail(k.email);
+         }
+       });
+   }, [props]);
+
   return (
       <>
         <h3>My Dashboard</h3>
